02144ef584a544d8ffd9e45a8bed512d09c20ed2
SungSuSon <thstjdtn05@gmail.com>
2023-06-26 09:34:06 +0900

    :bug:Fix: set redirect uri #3

--- static/js/api.js
+++ static/js/api.js
@@ -1,9 +1,9 @@
-export const FRONT_BASE_URL = "http://127.0.0.1:5500"
+export const FRONT_BASE_URL = "http://127.0.0.1:5501"
 export const BACK_BASE_URL = "http://127.0.0.1:8000"
 // export const BACK_BASE_URL = "http://127.0.0.1"
 // export const BACK_BASE_URL = "https://backend.chocothecoo.com"
 // export const REDIRECT_URI = FRONT_BASE_URL
-export const REDIRECT_URI = `${FRONT_BASE_URL}/login.html`
+export const REDIRECT_URI = `${FRONT_BASE_URL}/index.html`
 export const access_token = localStorage.getItem("access")
 export const payload = JSON.parse(localStorage.getItem("payload"))
 
