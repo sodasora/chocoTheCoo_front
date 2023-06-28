36999f3bb0ff0984632f5c23f16d8439b51d8a6e
Kim Gwang woon <misili304@naver.com>
2023-06-28 19:40:12 +0900

    :sparkles:Feat: header responsive #1

--- static/js/sellerpage.js
+++ static/js/sellerpage.js
@@ -1,4 +1,4 @@
-import { getProductslist, viewProductslist, getSellerProductListAPIView, BACK_BASE_URL, FRONT_BASE_URL, getSellerPermissionAPIView } from './api.js';
+import { getProductslist, viewProductslist, getSellerProductListAPIView, BACK_BASE_URL, FRONT_BASE_URL, getSellerPermissionAPIView, payload } from './api.js';
 
 export async function productDetail(product_id) {
   window.location.href = `${FRONT_BASE_URL}/productdetail.html?product_id=${product_id}`
@@ -7,11 +7,8 @@ export async function productDetail(product_id) {
 //등록한 상품들 전체 보기
 export async function sellerPageAPI() {
   try {
-    const payload = localStorage.getItem("payload");
-    const payload_parse = JSON.parse(payload);
-    const user_id = payload_parse.user_id //로그인한 유저id
+    const user_id = payload.user_id //로그인한 유저id
     const product = await getSellerProductListAPIView(user_id);
-    // console.log(product)
 
     if (product.count != 0) {
       if ((product.next == null) & (product.previous == null)) {
